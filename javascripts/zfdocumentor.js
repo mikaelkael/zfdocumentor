64a41c367693e4082b75e0c79df3747303f73f9b
Mickael Perraud <perraud.mickael@orange.fr>
2010-12-30 17:11:27 +0100

    ZFDocumentor: remove relative paths

--- javascripts/zfdocumentor.js
+++ javascripts/zfdocumentor.js
@@ -11,15 +11,3 @@ $(".version").click(function(){
   return false;
 });
 $(function() {$(".col2").load('toc.html');});
-SyntaxHighlighter.autoloader(
-  'plain text txt ini apache dos dosini yaml output lighttpd json conf ../../scripts/shBrushPlain.js',
-  'js jscript javascript  ../../scripts/shBrushJScript.js',
-  'php                    ../../scripts/shBrushPhp.js',
-  'bash shell sh          ../../scripts/shBrushBash.js',
-  'sql SQL querystring    ../../scripts/shBrushSql.js',
-  'xml xhtml xslt html xhtml ../../scripts/shBrushXml.js',
-  'as ActionScript        ../../scripts/shBrushAS3.js',
-  'java                   ../../scripts/shBrushJava.js',
-  'css                    ../../scripts/shBrushCss.js'
-);
-SyntaxHighlighter.all();
